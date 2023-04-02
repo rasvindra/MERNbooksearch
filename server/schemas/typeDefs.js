88456e544bd079eb65f0949709d0b5c417f4d9cc
Ravi Nagi <rasvindra@hotmail.com>
2023-04-02 16:00:23 -0700

    finished server side. unsure about bookCount in typedefs. its not in the model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,44 @@
 const typeDefs = `#graphql
+    type User {
+     _id: ID
+     username: String
+     email: String
+     bookCount: Num
+     password: String
+     savedBooks: [bookSchema]!
+    },
 
+    type Book {
+     _id: ID
+     authors: [String]
+     description: String
+     bookId: String
+     image: String
+     link: String
+     title: String
+    }
+
+    type Auth {
+     token: ID!
+     user: User
+    }
+
+    type Query {
+     users: [User]
+     user(username: String!): User
+     books(username: String): [Book]
+     book(bookId: ID!): Book
+     me: User
+    
+    }
+
+    type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(authors: String!, description: String!, bookId: String!, image: String!, link: String!, title: String! ): User
+    removeBook(bookId: ID!): User
+   
+  }
 
 
 `;
